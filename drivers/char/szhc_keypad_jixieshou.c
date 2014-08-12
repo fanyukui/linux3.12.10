03485708aa49c9e40aeeae11b6dd8edaa998572d
fanyukui <ying_593254979@126.com>
2014-08-12 18:08:40 +0800

    添加input键盘驱动

--- drivers/char/szhc_keypad_jixieshou.c
+++ drivers/char/szhc_keypad_jixieshou.c
@@ -18,46 +18,183 @@ COPYRIGHT:SZHC
 #include <linux/poll.h>
 #include <asm/atomic.h>
 #include <linux/gpio.h>
-
+#include <linux/input.h>
 
 
 /* 加载模式后,执行”cat /proc/devices”命令看到的设备名称 */
 #define DEVICE_NAME "szhc_keypads"
 
-#define KEYPAD_MAJOR 235 /* 主设备号 */
+
+#define ColumnCount   6
+#define RowCount   5
+
+char keypad[RowCount][ColumnCount];
+unsigned int columtable[ColumnCount];
+unsigned int rowtable[RowCount];
+
+unsigned int knob[3] = {80,81,82};
+unsigned int pulley[3] = {90,91,92};
 
 
 #define GPIO_TO_PIN(bank, gpio) (32 * (bank) + (gpio))
 
-struct timer_list s_timer;
+static struct timer_list s_timer;
+static struct input_dev  *button_dev; /*输入设备结构体*/
 
-void initPin(unsigned int bank,unsigned int gpio)
+void initKeyPad()
+{
+    int start = 1,i,j;
+    for(i=0;i<RowCount;i++)
+    {
+        for(j=0;j<ColumnCount;j++)
+        {
+            keypad[i][j] = start++;
+        }
+
+    }
+
+}
+
+void initPinOut(unsigned int gpio,bool isOut,char *label)
 {
     int result;
     /* Allocating GPIOs and setting direction */
-    result = gpio_request(GPIO_TO_PIN(bank,gpio), "Keypad");//usr1
+    result = gpio_request(gpio, label);//usr1
     if (result != 0)
-        printk("gpio_request(%d_%d) failed!\n",bank,gpio);
-    result = gpio_direction_input(GPIO_TO_PIN(bank,gpio));
-    if (result != 0)
-        printk("gpio_direction(%d_%d) failed!\n",bank,gpio);
+        printk("gpio_request(%d) failed!\n",gpio);
+    if(isOut){
+        result = gpio_direction_output(gpio,1);
+        if (result != 0)
+            printk("gpio_direction(%d) failed!\n",gpio);
+
+    }
+    else{
+        result = gpio_direction_input(gpio);
+        if (result != 0)
+            printk("gpio_direction(%d) failed!\n",gpio);
+    }
+
 }
+
 
 void initConfig(void)
 {
-	initPin(0,13); //停止
-	initPin(2,5);  //自动 低电平有效
-	initPin(2,2);  //手动
+    int row,column;
+    /*旋钮 输入*/
+	initPinOut(GPIO_TO_PIN(0,13),false,"Knob"); //停止
+	initPinOut(GPIO_TO_PIN(2,5),false,"Knob");  //自动 低电平有效
+	initPinOut(GPIO_TO_PIN(2,2),false,"Knob");  //手动
+
+    /*初始化 rowtable*/
+    rowtable[0] = GPIO_TO_PIN(3,14);
+    rowtable[1] = GPIO_TO_PIN(3,15);
+    rowtable[2] = GPIO_TO_PIN(3,16);
+    rowtable[3] = GPIO_TO_PIN(3,17);
+    rowtable[4] = GPIO_TO_PIN(3,18);
+  //  rowtable[5] = GPIO_TO_PIN(3,19);
+   // rowtable[6] = GPIO_TO_PIN(3,20);
+   // rowtable[7] = GPIO_TO_PIN(3,21);
+    /*行 输入*/
+    for(row=0;row<RowCount;row++)
+    {
+        initPinOut(rowtable[row],false,"KeyPadInput");
+    }
+
+    /*初始化 columntable*/
+    columtable[0] = GPIO_TO_PIN(1,27);
+    columtable[1] = GPIO_TO_PIN(1,26);
+    columtable[2] = GPIO_TO_PIN(1,25);
+    columtable[3] = GPIO_TO_PIN(0,31);
+    columtable[4] = GPIO_TO_PIN(1,17);
+    columtable[5] = GPIO_TO_PIN(3,19);
+  //  columtable[5] = GPIO_TO_PIN(1,16);
+  //  columtable[6] = GPIO_TO_PIN(0,30);
+  //  columtable[7] = GPIO_TO_PIN(1,28);
+
+    /*列 输出*/
+    for(column=0;column<ColumnCount;column++)
+    {
+        initPinOut(columtable[column],true,"KeyPadOutput");
+    }
 
 }
+
+
+/*拉低指定列*/
+void downColumn(int index)
+{
+   int i;
+   for(i=0;i<ColumnCount;i++)
+   {
+        if(i==index)
+            gpio_set_value(columtable[i],0);
+        else
+            gpio_set_value(columtable[i],1);
+   }
+
+}
+
+
 void timer_handler(unsigned long arg)
 {
-	printk("GPIO_TO_PIN(0,13): %d\n",gpio_get_value(GPIO_TO_PIN(0,13)));
-  	printk("GPIO_TO_PIN(2,5) : %d\n",gpio_get_value(GPIO_TO_PIN(2,5)));
-	printk("GPIO_TO_PIN(2,2) : %d\n",gpio_get_value(GPIO_TO_PIN(2,2)));
+    int col = 0,row = 0;
+    int pressed = 0;
+
+    /*处理旋钮*/
+  	int p1 = gpio_get_value(GPIO_TO_PIN(0,13));
+    int p2 = gpio_get_value(GPIO_TO_PIN(2,5));
+    int p3 = gpio_get_value(GPIO_TO_PIN(2,2));
+    int value = (p1 << 2) | (p2 << 1) | p3;
+    //printk("value: %d\n",value);
+    if(value == 6){
+  			input_report_key(button_dev,knob[0], true);
+  			input_report_key(button_dev,knob[1], false);
+  			input_report_key(button_dev,knob[2], false);
+            input_sync(button_dev);
+    }
+    else if(value == 3){
+  			input_report_key(button_dev,knob[1], true);
+        	input_report_key(button_dev,knob[0], false);
+        	input_report_key(button_dev,knob[2], false);
+
+            input_sync(button_dev);
+
+    }
+    else if(value == 1){
+  			input_report_key(button_dev,knob[2], true);
+        	input_report_key(button_dev,knob[0], false);
+        	input_report_key(button_dev,knob[1], false);
+            input_sync(button_dev);
+
+    }
+
+    /*处理普通按键*/
+    for(col=0;col<ColumnCount;col++)
+    {
+        downColumn(col);
+        for(row=0;row<RowCount;row++)
+        {
+            while(gpio_get_value(rowtable[row]) == 0)
+            {
+                pressed = 1;
+              //  printk("row: %d\tcolumn:%d\tkey value: %d \t pressed!\n",row,col,keypad[row][col]);
+      			input_report_key(button_dev,keypad[row][col], pressed);
+                input_sync(button_dev);
+            }
+            if(pressed){
+                pressed = 0;
+             //   printk("row: %d\tcolumn:%d\tkey value: %d \t released!\n",row,col,keypad[row][col]);
+      			input_report_key(button_dev,keypad[row][col], pressed);
+                input_sync(button_dev);
+            }
+
+        }
+
+    }
+
 
 	unsigned long j = jiffies;
-	s_timer.expires = j + HZ ;
+	s_timer.expires = j + HZ/100 ;
 	add_timer(&s_timer);
 }
 
@@ -95,46 +232,72 @@ static struct class *keypad_class;
 
 static int __init szhc_keypad_init(void)
 {
-    int ret,i;
+    int error;
     printk(banner);
 
-    /*初始化LED配置*/
+    /*初始化KeyPad配置*/
     initConfig();
+
+    /*初始化KeyPad值*/
+    initKeyPad();
+
+    button_dev = input_allocate_device(); /*分配一个设备结构体*/
+    if(!button_dev)
+    {
+        printk("Not enough memory\n");
+        error = -ENOMEM;
+        goto error_free_dev;
+    }
+
+    __set_bit(EV_KEY,button_dev->evbit);//支持EV_KEY事件
+    //__set_bit(EV_REP,button_dev->evbit); //支持EV_REP事件
+
+    //button_dev->evbit[0] = BIT_MASK(EV_KEY);  //支持EV_KEY事件
+    //button_dev->evbit[0] = BIT_MASK(EV_REP);  //支持EV_REP事件
+
+    int i,j;
+    for(i=0; i<RowCount;i++)
+    {
+        for(j=0;j<ColumnCount;j++)
+        {
+            //支持普通键
+	    	__set_bit(keypad[i][j], button_dev->keybit);
+        }
+    }
+    /*支持旋钮*/
+    for(i=0; i<3;i++)
+        __set_bit(knob[i], button_dev->keybit);
+
+    /*支持滑轮*/
+    for(i=0;i<3;i++)
+        __set_bit(pulley[i],button_dev->keybit);
+
+
+  	error = input_register_device(button_dev);
+
+    if(error){
+        printk("failed to register device\n");
+        goto error_free_dev;
+
+    }
 
     /*初始化Timer配置*/
     initTimer();
 
-    /* 注册字符设备驱动程序
-     * 参数为主设备号、设备名字、file_operations 结构;
-     * 这样,主设备号就和具体的 file_operations 结构联系起来了,
-     * 操作主设备为 LED_MAJOR 的设备文件时,就会调用 EmbedSky_leds_fops 中的相关成员函数
-     * LED_MAJOR 可以设为 0,表示由内核自动分配主设备号
-     */
-    ret = register_chrdev(KEYPAD_MAJOR, DEVICE_NAME, &szhc_keypad_fops);
-    if (ret < 0)
-    {
-        printk(DEVICE_NAME " can't register major number\n");
-        return ret;
-    }
-    //注册一个类,使 mdev 可以在"/dev/"目录下面建立设备节点
-    keypad_class = class_create(THIS_MODULE, DEVICE_NAME);
-    if(IS_ERR(keypad_class))
-    {
-        printk("Err: failed in EmbedSky-leds class. \n");
-        return -1;
-    }
-    //创建一个设备节点,节点名为 DEVICE_NAME
-    device_create(keypad_class, NULL, MKDEV(KEYPAD_MAJOR, 0), NULL, DEVICE_NAME);
-
     return 0;
+
+    error_free_dev:
+        input_free_device(button_dev);
+
+    return error;
+
 }
 
 static void __exit szhc_keypad_exit(void)
 {
     /* 卸载驱动程序 */
-    unregister_chrdev(KEYPAD_MAJOR, DEVICE_NAME);
-    device_destroy(keypad_class, MKDEV(KEYPAD_MAJOR, 0)); //删掉设备节点
-    class_destroy(keypad_class); 		//注销类
+    input_unregister_device(button_dev);
+
 }
 
 /* 这两行指定驱动程序的初始化函数和卸载函数 */
