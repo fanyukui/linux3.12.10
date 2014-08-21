45c10d210c3f953274a736c3920adaa45a9e6d92
fanyukui <ying_593254979@126.com>
2014-08-21 18:50:36 +0800

    添加滚轮驱动

--- drivers/char/szhc_keypad_jixieshou.c
+++ drivers/char/szhc_keypad_jixieshou.c
@@ -19,7 +19,8 @@ COPYRIGHT:SZHC
 #include <asm/atomic.h>
 #include <linux/gpio.h>
 #include <linux/input.h>
-
+#include <linux/irq.h>
+#include <linux/interrupt.h>
 
 /* 加载模式后,执行”cat /proc/devices”命令看到的设备名称 */
 #define DEVICE_NAME "szhc_keypads"
@@ -76,15 +77,43 @@ void initPinOut(unsigned int gpio,bool isOut,char *label)
 
 }
 
+irqreturn_t irq_handler(int irqno, void *dev_id)
+
+{
+    printk("GPIO_TO_PIN(2,4) :%d\n",gpio_get_value(GPIO_TO_PIN(2,4)));
+    return IRQ_HANDLED;
+}
+
 
 void initConfig(void)
 {
     int row,column;
+    int irq = 0,ret = 0;
     /*旋钮 输入*/
 	initPinOut(GPIO_TO_PIN(0,13),false,"Knob"); //停止
 	initPinOut(GPIO_TO_PIN(2,5),false,"Knob");  //自动 低电平有效
 	initPinOut(GPIO_TO_PIN(2,2),false,"Knob");  //手动
 
+    /*滚轮 输入*/
+    initPinOut(GPIO_TO_PIN(2,1),false,"pulley");
+    initPinOut(GPIO_TO_PIN(2,3),false,"pulley");
+    initPinOut(GPIO_TO_PIN(2,4),false,"pulley");
+
+    /*申请中断*/
+    irq = gpio_to_irq(GPIO_TO_PIN(2, 3));
+    if(irq < 0){
+        printk("GPIO_TO_PIN(2, 3) irq  failed !\n");
+        ret = irq;
+    }
+    printk("irq = %d\n", irq);
+    ret = request_irq(irq,
+                    irq_handler,
+                    IRQF_TRIGGER_FALLING | IRQF_SHARED,
+                    "pulley",
+                    irq_handler);
+    if(ret){
+        printk("request_irq() failed ! %d\n", ret);
+    }
     /*初始化 rowtable*/
     rowtable[0] = GPIO_TO_PIN(3,14);
     rowtable[1] = GPIO_TO_PIN(3,15);
@@ -148,22 +177,27 @@ void timer_handler(unsigned long arg)
     //printk("value: %d\n",value);
     if(value == 6){
   			input_report_key(button_dev,knob[0], true);
-  			input_report_key(button_dev,knob[1], false);
-  			input_report_key(button_dev,knob[2], false);
+  			input_report_key(button_dev,knob[0], false);
             input_sync(button_dev);
     }
     else if(value == 3){
   			input_report_key(button_dev,knob[1], true);
-        	input_report_key(button_dev,knob[0], false);
-        	input_report_key(button_dev,knob[2], false);
-
+  			input_report_key(button_dev,knob[1], false);
             input_sync(button_dev);
 
     }
     else if(value == 1){
   			input_report_key(button_dev,knob[2], true);
-        	input_report_key(button_dev,knob[0], false);
-        	input_report_key(button_dev,knob[1], false);
+  			input_report_key(button_dev,knob[2], false);
+            input_sync(button_dev);
+
+    }
+
+    /*处理滚轮按下事件*/
+    if(gpio_get_value(GPIO_TO_PIN(2,1)) == 0)
+    {
+  			input_report_key(button_dev,pulley[0], true);
+  			input_report_key(button_dev,pulley[0], false);
             input_sync(button_dev);
 
     }
@@ -228,7 +262,7 @@ static struct file_operations szhc_keypad_fops =
     //.compat_ioctl = szhc_leds_ioctl,
 };
 static char __initdata banner[] = "SZHCAM335x  Keypad, (c) 2014 SZHC\n";
-static struct class *keypad_class;
+//static struct class *keypad_class;
 
 static int __init szhc_keypad_init(void)
 {
@@ -298,7 +332,10 @@ static void __exit szhc_keypad_exit(void)
     /* 卸载驱动程序 */
   	del_timer(&s_timer);
     input_unregister_device(button_dev);
-
+    /* free gpio */
+    int irq = gpio_to_irq(GPIO_TO_PIN(2, 3));
+    free_irq(irq,irq_handler) ;
+    //gpio_free(GPIO_TO_PIN(1, 27));
 }
 
 /* 这两行指定驱动程序的初始化函数和卸载函数 */
